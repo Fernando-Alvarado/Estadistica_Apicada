475773228065ee4c8904f69d1e9b77327e59cf3d
Fernando Alvarado <fernandoalvarado@ciencias.unam.mx>
2025-03-03 00:40:42 -0600

    subiendo avance bayesiana

--- Estadistica_Bayesiana/Tarea_2/main.r
+++ Estadistica_Bayesiana/Tarea_2/main.r
@@ -2,6 +2,8 @@
 #Autor: Fernando Alvarado Palacios
 #Fecha: 2021/09/15
 
+library(ggplot2)
+library(tidyr)
 #Inciso A
 alpha <- 49 / 196
 beta <- 7 / 196
@@ -27,3 +29,32 @@ print(1 - p_b)
 
 
 #Graficas 
+
+observaciones <- c(879, 903, 948, 939, 893) #Dtos que nos da las observaciones
+
+#Graficas
+valores = seq(0, 1500, length.out = 750)
+
+# Crear data frame con la distribución gamma inicial
+df_sim <- data.frame(
+  x = valores,
+  priori = dgamma(valores, shape = alpha, rate = beta)
+)
+
+# Agregar columnas de observaciones iterativamente
+for (i in 1:5) {
+  df_sim[, paste0("obs_", i)] <- dgamma(df_sim$x, 
+                                        shape = 1 * (alpha + sum(observaciones[1:i])),
+                                        rate = beta + i)
+}
+
+df_mod <- df_sim %>%
+  pivot_longer(cols = -x, names_to = "grupo", values_to = "y")
+
+head(df_mod)
+
+# Mostrar gráfico
+#x11()  # Abrir ventana de gráficos (opcional)
+ggplot(df_mod, aes(x = x, y = y, color = grupo)) +
+  geom_line() +
+  theme_minimal()
